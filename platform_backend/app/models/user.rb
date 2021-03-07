2bdb6eef005f7df1827ce4852583d8c126d1acd2
sujit_jaiwaliya <sujit.jaiwaliya@gocomet.com>
2021-03-07 12:54:42 +0530

    validate user model

--- platform_backend/app/models/user.rb
+++ platform_backend/app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+	validates :name, presence: true
+	validates :phone_number, length: {is: 10}, allow_blank: false
+	validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
 end
